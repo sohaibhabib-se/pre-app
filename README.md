4a0c63a00133bb720e5a691e08e73be15345fac1
sohaibhabib-se <sohaibhabib8@gmail.com>
2026-02-22 00:59:13 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pracctial React Essentials by Muhammad Ahsan Ayaz
+
+This is the course practicegit init
+
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='mb-4'>Hello Sohaib!!!</h1>
+
+      <p>Senior Consultant Mobile Development</p>
+    </>
+  )
+}
+
+export default App
