e5b2890778bfb21f1febfe8f4cffb5a9d1e6aa96
Muhammad Sohaib <m.sohaib@systemsltd.com>
2026-04-01 21:57:40 +0500

    Counter completed using states and props lecture

--- src/componenets/Counter.tsx
+++ src/componenets/Counter.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import { useState } from 'react';
 
 const Counter = () => {
+
+  const [counterValue, setCounterValue] = useState(10);
+
+  const increment = () => {
+    setCounterValue(counterValue + 1);
+  }
+
+  const decrement = () => {
+    if (counterValue == 0) {
+      setCounterValue(0);
+    } else {
+      setCounterValue(counterValue - 1);
+    }
+  }
+
+  const reset = () => {
+    setCounterValue(0);
+  }
   return (
     <section className='mb-10 flex flex-col'>
         <h2 className='text-2xl'>Count is</h2>
-        <p className='text-xl'>0</p>
+        <p className='text-xl'>{counterValue}</p>
         <div className='flex gap-4 mb-4 justify-center'>
-            <button> - Decrement</button>
-            <button> + Increment</button>
+            <button onClick={decrement}> - Decrement</button>
+            <button onClick={increment}> + Increment</button>
         </div>
         <div className='flex justify-center'>
-            <button>Reset</button>
+            <button onClick={reset}>Reset</button>
         </div>
     </section>
   )
